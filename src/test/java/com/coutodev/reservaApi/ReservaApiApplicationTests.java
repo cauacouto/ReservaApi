d96a153a0ec9841481d67237d82a6ef3d46ca101
caua <cauacouto022@gmail.com>
2025-12-04 23:39:51 -0300

    first commit

---
+++ src/test/java/com/coutodev/reservaApi/ReservaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coutodev.reservaApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReservaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
